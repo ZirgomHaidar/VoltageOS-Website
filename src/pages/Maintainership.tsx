f0cf162a7fe603ab707ab6326aec491e868dc92c
Frost <frost.github@proton.me>
2025-10-30 13:52:09 +0530

    VOS: Adapt for new maintainership repo

--- src/pages/Maintainership.tsx
+++ src/pages/Maintainership.tsx
@@ -25,19 +25,19 @@ function Maintainership() {
     >
       <div className="mb-14 items-center space-y-2 text-center lg:mb-28">
         <h1 className="mb-8 font-bold tracking-wide">Maintainership</h1>
-        <h3>Can"t Find Your Device? Apply for your device and Join us</h3>
+        <h3>Can't Find Your Device? Apply for your device and join us</h3>
         <h6>
-          Fore more info join{" "}
+          For more info join{" "}
           <Link to="https://t.me/VoltageOSSupport">
             <b className="underline-offset-8 transition-all hover:text-xl hover:underline">
-              VoltageOS Support
+              VoltageOS Build Support
             </b>
           </Link>
         </h6>
       </div>
 
       <div className="bg-Voltage-bgComponent text-Voltage-textSecondary border-Voltage-borderComponent rounded-4xl border-3 px-10 py-5 lg:mx-8">
-        <h2>Basic criteria for application are:</h2>
+        <h2>Application Info:</h2>
         <motion.ul
           className="markerstyle mt-6 list-inside space-y-4 lg:text-xl"
           variants={container}
@@ -50,9 +50,10 @@ function Maintainership() {
             </motion.li>
           ))}
         </motion.ul>
+
         <div className="mt-6 flex">
           <Link
-            to="https://forms.gle/eSNQrRzi2SrKqV9Y7"
+            to="https://github.com/VoltageOS/maintainership"
             className="grow text-center text-xl"
           >
             <motion.button
@@ -60,7 +61,7 @@ function Maintainership() {
               whileHover={{ scale: 1.02 }}
               whileTap={{ scale: 0.96 }}
             >
-              Maintainers Form
+              Apply for Maintainership
             </motion.button>
           </Link>
         </div>
@@ -72,9 +73,5 @@ function Maintainership() {
 export default Maintainership
 
 const Rules = [
-  "Applicants are permitted to maintain only one custom ROM, whether it is official or unofficial, in conjunction with VoltageOS.",
-  "A fundamental understanding of Git is essential for all applicants.",
-  "Candidates should possess the ability to independently troubleshoot and resolve device-related issues and bugs that may arise.",
-  "It is required that applicants own the specific device for which they are submitting their application.",
-  "Additionally, applicants must have maintained an unofficial VoltageOS build for a minimum of 2 months, ensuring that regular monthly updates are provided throughout that period.",
+  "All maintainer requirements and the Code of Conduct are now available on our GitHub repository.",
 ]
