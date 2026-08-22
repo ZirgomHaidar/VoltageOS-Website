044a22e0d14b8a6e0ad33fc3492b2ade2e1e3d37
Frost <frost.github@proton.me>
2026-08-22 15:13:43 +0530

    VOS: Update URLs for Android 17

--- src/components/ui/SpringModal.tsx
+++ src/components/ui/SpringModal.tsx
@@ -40,7 +40,7 @@ const SpringModal = ({
   const fetchChangelog = async () => {
     try {
       const response = await fetch(
-        `https://raw.githubusercontent.com/VoltageOS/android_vendor_voltageota/refs/heads/16.2/changelog_${codename}.txt`,
+        `https://raw.githubusercontent.com/VoltageOS/android_vendor_voltageota/refs/heads/17/changelog_${codename}.txt`,
       )
       if (!response.ok) {
         throw new Error("Network response was not ok")
